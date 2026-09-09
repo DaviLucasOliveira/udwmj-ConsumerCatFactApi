cbf170e8c0875cac85b5a087019152a6edccfe66
Davi Lucas dos Santos Oliveira <56208920+DaviLucasOliveira@users.noreply.github.com>
2026-09-09 20:46:02 -0300

    Add files via upload

---
+++ ConsumerGatinho/CatFact.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsumerGatinho
+{
+    public class CatFact
+    {
+        public string? fact { get; set; }
+        public int? length { get; set; }
+    }
+}
---
+++ ConsumerGatinho/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsumerGatinho;
+using static System.Console;
+
+var enderecoUrl = "https://catfact.ninja/fact";
+
+WriteLine($"Consultando o endereço na url: {enderecoUrl}");
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage response = await client.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    string respostaApi = await response.Content.ReadAsStringAsync();
+
+    CatFact? catFact = System.Text.Json.JsonSerializer.Deserialize<CatFact>(respostaApi);
+
+    WriteLine($"\nCat fact: {catFact?.fact}");
+    WriteLine($"Lenght: {catFact?.length} \n");
+
+}
+catch (Exception e)
+{
+    WriteLine($"Ocorreu um erro ao consultar o endereço: {e.Message}");
+}
